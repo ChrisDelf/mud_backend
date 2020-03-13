e8ff22f5b850ddf83c63deb75d0754ea834e11e0
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-03-13 18:19:04 -0400

    Adding Oauth configs files

---
+++ src/main/java/com/dev/mud_backend/models/User.java
@@ -0,0 +1,152 @@
+package com.dev.mud_backend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User extends Auditable
+ {
+     @Id
+     @GeneratedValue(strategy = GenerationType.AUTO)
+     private long userid;
+
+
+     @ApiModelProperty(notes = "Unique username is required", example = "Bobby", required = true)
+     @Column(nullable = false,
+             unique = true)
+     private String username;
+
+     @ApiModelProperty(notes = "Password is required", example = "password", required = true)
+     @Column(nullable = false)
+     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+     private String password;
+
+     @OneToMany(mappedBy = "user",
+             cascade = CascadeType.ALL)
+     @JsonIgnoreProperties("user")
+     private List<UserRoles> userroles = new ArrayList<>();
+
+     @ApiModelProperty(notes = "email is not required", example = "email@email.com", required = false)
+     @OneToMany(mappedBy = "user",
+             cascade = CascadeType.ALL,
+             orphanRemoval = true)
+     @JsonIgnoreProperties("user")
+     private List<Useremail> useremails = new ArrayList<>();
+
+     @OneToMany(mappedBy = "user",
+             cascade = CascadeType.ALL,
+             orphanRemoval = true  )
+     @JsonIgnoreProperties("user")
+     private List<UserWorkout> userworkouts = new ArrayList<>();
+
+
+
+     public User()
+     {
+     }
+
+     public User(String username, String password, List<UserRoles> userRoles)
+     {
+         setUsername(username);
+         setPassword(password);
+         for (UserRoles ur : userRoles)
+         {
+             ur.setUser(this);
+         }
+         this.userroles = userRoles;
+     }
+
+     public List<UserWorkout> getUserworkouts() {
+         return userworkouts;
+     }
+
+     public void setUserworkouts(List<UserWorkout> userworkouts) {
+         this.userworkouts = userworkouts;
+     }
+
+     public long getUserid()
+     {
+         return userid;
+     }
+
+     public void setUserid(long userid)
+     {
+         this.userid = userid;
+     }
+
+     public String getUsername()
+     {
+         return username;
+     }
+
+     public void setUsername(String username)
+     {
+         this.username = username;
+     }
+
+     public String getPassword()
+     {
+         return password;
+     }
+
+     public void setPassword(String password)
+     {
+         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+         this.password = passwordEncoder.encode(password);
+     }
+
+     public void setPasswordNoEncrypt(String password)
+     {
+         this.password = password;
+     }
+
+     public List<UserRoles> getUserroles()
+     {
+         return userroles;
+     }
+
+     public void setUserroles(List<UserRoles> userroles)
+     {
+         this.userroles = userroles;
+     }
+
+     public List<Useremail> getUseremails()
+     {
+         return useremails;
+     }
+
+     public void setUseremails(List<Useremail> useremails)
+     {
+         this.useremails = useremails;
+     }
+
+     public List<SimpleGrantedAuthority> getAuthority()
+     {
+         List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+         for (UserRoles r : this.userroles)
+         {
+             String myRole = "ROLE_" + r.getRole()
+                     .getName()
+                     .toUpperCase();
+             rtnList.add(new SimpleGrantedAuthority(myRole));
+         }
+
+         return rtnList;
+     }
+
+
+     @Override
+     public String toString()
+     {
+         return "User{" + "userid=" + userid + ", username='" + username + '\'' + ", password='" + password + '\'' + ", userRoles=" + userroles + ", useremails=" + useremails + '}';
+     }
+}
