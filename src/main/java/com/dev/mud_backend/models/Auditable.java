f55d6de3237fff4faa962dbd88ec89301f288b4a
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-03-16 18:44:36 -0400

    Added user models

---
+++ src/main/java/com/dev/mud_backend/models/Auditable.java
@@ -0,0 +1,27 @@
+package com.dev.mud_backend.models;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Temporal;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+abstract class Auditable {
+    @CreatedBy
+    protected String createdBy;
+
+    @CreatedDate
+    @Temporal(TIMESTAMP)
+    protected Date createdDate;
+
+    @LastModifiedBy
+    protected String lastModifiedBy;
+
+    @LastModifiedDate
+    @Temporal(TIMESTAMP)
+    protected Date lastModifiedDate;
+}
