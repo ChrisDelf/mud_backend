c492542bff27f61e84f859853b2813c9fc3c8f82
Chris <chrisdel@protonmail.com>
2020-06-12 08:29:25 -0400

    added the repos, services for monsters and items

--- src/main/java/com/dev/mud_backend/services/ItemsServiceImpl.java
+++ src/main/java/com/dev/mud_backend/services/ItemsServiceImpl.java
@@ -1,4 +1,21 @@
 package com.dev.mud_backend.services;
 
+import com.dev.mud_backend.models.Item;
+
 public class ItemsServiceImpl implements ItemsService {
+
+    @Override
+    public Long SellItem(long id) {
+        return null;
+    }
+
+    @Override
+    public void DestroyItem(long id) {
+
+    }
+
+    @Override
+    public Item DropItem(long id) {
+        return null;
+    }
 }
--- src/main/java/com/dev/mud_backend/services/MonsterServiceImpl.java
+++ src/main/java/com/dev/mud_backend/services/MonsterServiceImpl.java
@@ -1,4 +1,25 @@
 package com.dev.mud_backend.services;
 
+import com.dev.mud_backend.models.Monster;
+
 public class MonsterServiceImpl implements MonsterService  {
+    @Override
+    public Monster MonsterStats(long id) {
+        return null;
+    }
+
+    @Override
+    public long MonsterAttack(long monsterId, long playerId) {
+        return 0;
+    }
+
+    @Override
+    public long MonsterHeal(long monsterId) {
+        return 0;
+    }
+
+    @Override
+    public String MonsterDeath(long monsterId) {
+        return null;
+    }
 }
