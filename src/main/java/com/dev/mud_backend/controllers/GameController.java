e417ed0b64e7236d2212f0113e074e5f385dc37a
Chris <chrisdel@protonmail.com>
2020-06-12 10:32:43 -0400

    Created the base endpoints for the player's actions

--- src/main/java/com/dev/mud_backend/controllers/GameController.java
+++ src/main/java/com/dev/mud_backend/controllers/GameController.java
@@ -3,6 +3,10 @@ package com.dev.mud_backend.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,17 +21,43 @@ public class GameController {
     // update
     /// /player/pickupItem/
 
-    // update
-    // /player/dropItem/
+    @PutMapping(value ="/player/itemInteraction", produces = {"application/json"})
+    public ResponseEntity<?> playerItemInteraction(long itemId){
+
+
+
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
     //Get
     // /player/checkInventory/
+    @GetMapping(value ="/player/checkInventory", produces = {"application/json"})
+    public ResponseEntity<?> playerCheckInventory(long playerId) {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
 
     //Get
-    // /player/inspect
+    // /player/insight
+    @GetMapping(value ="/player/insight", produces = {"applications/json"})
+    public ResponseEntity<?>  playerInspect(long id){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
 
     // update
     // /player/combatAction
+    @PutMapping(value ="player/combatAction", produces = {"application/json"})
+    public  ResponseEntity<?> playerCombat(long monserId){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 
 
